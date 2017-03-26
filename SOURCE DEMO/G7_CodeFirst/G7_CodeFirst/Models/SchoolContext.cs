0b27548fc5bbcf47e54dd76fe93a3a593d331808
sikyurabmt <sikyurabmt@gmail.com>
2017-03-26 10:40:40 +0700

    Up source + tutorial Code first, database first

---
+++ SOURCE DEMO/G7_CodeFirst/G7_CodeFirst/Models/SchoolContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace G7_CodeFirst.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext()
+            : base("name=SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+ 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // khai báo Id sẽ là khóa chính
+            modelBuilder.Entity<Student>().HasKey(b => b.Id);
+ 
+            // khai báo Id sẽ tự động tăng
+            modelBuilder.Entity<Student>().Property(b => b.Id)
+            .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ SOURCE DEMO/G7_CodeFirst/G7_CodeFirst/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace G7_CodeFirst.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Major { get; set; }
+    }
+}
